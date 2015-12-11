ef2de9c1bc2bc47fcc6002beac9530be48fd9529
Phil D <e303t@e303-s18.lab.lagcc>
2015-12-10 21:05:19 -0500

    images

---
+++ Water Towers/main.js
@@ -0,0 +1,27 @@
+$(document).ready(
+  function(){
+    $("figure").on("click",function(){
+      addMyClass(this);
+      findMyImage(this);
+    }
+  )
+  }
+);
+/*add a border to clicked image */
+function addMyClass(item){
+  $("figure").removeClass("clicked");
+  $(item).addClass("clicked");
+};
+/* figure out the image we're looking for */
+function findMyImage(item){
+  var myKids = $(item).children();
+  var myImage = myKids[0];
+  var imageSrc = $(myImage).attr("src");
+  var regExPatt = /[0-9]/;
+  var regExItem = new RegExp(regExPatt);
+  var imageNum = regExItem.exec (imageSrc);
+  console.log(imageNum);
+  var myCaption = myKids[1];
+  var imageUrl = "images/watertowers-";
+  $("div#image img").attr("src",imageUrl + imageNum[0] +".jpg");
+}
